40e8a883addb2e49168653a83fdedc89990e537b
Dylan <122402729+advdylan@users.noreply.github.com>
2025-04-28 13:32:48 +0200

    Update JcalendarS.js

--- JcalendarS.js
+++ JcalendarS.js
@@ -1,59 +1,75 @@
-function getDaysOfAllMonths(year) {
-    let daysOfMonthsArray = [];
-    for (let month = 0; month < 12; month++) {
-        // Get the last day of the month by going to next month and day 0
-        let lastDay = new Date(year, month + 1, 0).getDate();
-        daysOfMonthsArray.push(lastDay);
-    }
-    return daysOfMonthsArray;
-}
-
 function milisecondsToDays(data) {
-    let newDate = (data/(1000*60*60*24)).toFixed(0);
-    return newDate;
-}
+		let newDate = (data/(1000*60*60*24)).toFixed(0);
+		return newDate;
+	}
 
 function getAllMonths() {
+		let months = [];
+		for (let i=0; i <= 11; i++) {
+			months.push(i);
+		}
+		console.log(months);
+		return months;
+	}
 
-    let months = [];
-    for (let i=0; i <= 11; i++) {
-        months.push(i);
-    }
-    console.log(months);
-    return months;
+function getDaysOfAllMonths(year) {
+		months = getAllMonths();
+		daysOfMonthsArray = [];
+		
+		for (let month of months) {
+			let firstDayOfMonth = new Date(year, month, 0)
+			let lastDayOfMonth = new Date(year, month + 1, 0 )
+			let newDate = lastDayOfMonth - firstDayOfMonth;
+			let formattedDate = milisecondsToDays(newDate);
+			daysOfMonthsArray.push(formattedDate);
+		}
+		console.log(daysOfMonthsArray);
+		return daysOfMonthsArray;
+	}
+	
+	
+class JCalendarS {
+	
+	constructor(year = new Date().getFullYear()) {
+		this.year = year;
+		this.data = [];
+
+	}
+	init() {
+			this.data = this.generateYearData();
+		}
+	
+	generateYearData() {
+		
+			let yearInput = document.getElementById('year');
+			let year = parseInt(yearInput.value);
+			console.log(year);
+			let daysOfMonthsArray = getDaysOfAllMonths(year);
+			console.log(`Daysof the months array: ${daysOfMonthsArray[0]}`);
+			
+			daysOfMonthsArray.forEach((value,index) => {
+				//console.log(`Value: ${value}`);
+				//console.log(`Month : ${index+1}`);
+				//Array of months? 
+				for (i=0; i < value; i++)
+				{
+					//console.log(i);
+					let date = new Date(year, index, i+1);
+					let newDate = createDateStructure(date);
+					data.push(newDate);
+					
+				}
+			});
+			 /*
+			for (let newDate of data) {
+				console.log(newDate.day);
+			} */
+			return data;
+		
+	}
 }
 
-class JcalendarS {
-    constructor(year = new Date().getFullYear()) {
-        this.year = year;
-        this.data = []
-    }
+let year = 1994;
 
-    init() {
-        this.data = this.generateYearData();
-    }
-
-    
-
-    generateYearData() {
-
-            let year = this.year
-            let months = getAllMonths();
-            let daysOfMonthsArray = getDaysOfAllMonths(year);
-            daysOfMonthsArray = [];
-            
-            for (let month of months) {
-                let firstDayOfMonth = new Date(year, month, 0)
-                let lastDayOfMonth = new Date(year, month + 1, 0 )
-                let newDate = lastDayOfMonth - firstDayOfMonth;
-                let formattedDate = milisecondsToDays(newDate);
-                daysOfMonthsArray.push(formattedDate);
-            }
-            console.log(daysOfMonthsArray);
-            return daysOfMonthsArray;
-    }
-}
-
-let newCalendar =  new JcalendarS(1994)
-newCalendar.init()
-console.log(newCalendar)
+let calendar = new JCalendarS(year);
+console.log(calendar);
